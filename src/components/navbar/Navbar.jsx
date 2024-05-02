d1be18c085bff1aff1440e1c24a61486bedcc575
dchung211 <128754322+dchung211@users.noreply.github.com>
2024-05-02 13:19:01 -0500

    Added Nav Links

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Links from "./links/Links";
+const navbar = () => {
+    return <div>
+        <div>Nav Bar</div>
+        <div>
+            <Links/>
+        </div>
+        </div>;
+  };
+  
+  export default navbar;
